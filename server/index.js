fc488b573b63ace9f845d8074fb0336f3f9a4753
harshitha042297 <HARSHITHAREDDY2297@GMAIL.COM>
2023-11-05 21:23:09 -0500

    changed .env

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 
 app.use('/posts',postRoutes)
 
-// const CONNECTION_URL='mongodb+srv://harshithareddy2297:Manhattan2297@cluster0.omhzedu.mongodb.net/';
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
